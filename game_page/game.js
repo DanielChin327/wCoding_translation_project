7717f2b5c8d81fcc04b695e465b693abe0d542f5
Daniel Chin <DanChin327@gmail.com>
2024-08-05 16:46:24 +0900

    refactor the question function and styling

--- game_page/game.js
+++ game_page/game.js
@@ -1,33 +1,47 @@
 let answer = ''; // Declare the answer variable globally
 let timer; // Declare timer variable globally to manage it properly
 let highScore = 0; // Declare high score variable globally
+let timerDisplay; // Declare timerDisplay globally to access it within functions
 
 document.addEventListener('DOMContentLoaded', function() {
-  let count = 30;
-  const timerDisplay = document.querySelector('#timer_display');
+  timerDisplay = document.querySelector('#timer_display');
   const startButton = document.querySelector('#start_button');
   const form = document.querySelector('form');
+  let resetButton = document.querySelector("#reset_button");
 
+  // How to save high score
+  if (localStorage.getItem('highScore')) {
+    highScore = parseInt(localStorage.getItem('highScore'));
+    document.querySelector('#high_score').innerText = highScore;
+  }
 
-  startButton.addEventListener('click', function() {
-    if (timer) {
-      clearInterval(timer); // Clear any existing timer
-    }
-    count = 30; // Reset the count (or set it to your desired duration)
-    timerDisplay.innerText = "00:30"; // Reset the display
-    startTimer(count, timerDisplay);
-    fetch('/game_page/al_bhed.json')
-      .then(response => response.json())
-      .then(data => {
-        let questionChoice = data[randomNumber(data)];
-        const questionClass = document.querySelector('.question');
-        questionClass.innerText = questionChoice.alBhed;
-        answer = questionChoice.english; // Update the global answer variable
-      })
-  });
-
+  startButton.addEventListener('click', makeQuestion);
   form.addEventListener('submit', answerQuestion);
+
+  resetButton.addEventListener('click', function() {
+    localStorage.removeItem('highScore'); // Remove high score from local storage
+    highScore = 0;
+    document.querySelector('#high_score').innerText = highScore; // Update the displayed high score
+  });
 });
+
+function makeQuestion() {
+  if (timer != 0) {
+    clearInterval(timer); // Clear any existing timer
+  }
+  let count = 5; // Reset the count (or set it to your desired duration)
+  timerDisplay.innerText = "00:05"; // Reset the display
+  startTimer(count, timerDisplay);
+  fetch('/game_page/al_bhed.json')
+    .then(response => response.json())
+    .then(data => {
+      let questionChoice = data[randomNumber(data)];
+      const questionClass = document.querySelector('.question');
+      questionClass.innerText = questionChoice.alBhed;
+      answer = questionChoice.english; // Update the global answer variable
+      document.querySelector('#input-area').focus(); // Auto sends the user to the text-input area to start typing right away.
+    });
+}
 
 function answerQuestion(event) {
   event.preventDefault();
@@ -41,11 +55,12 @@ function answerQuestion(event) {
   } else if (answer === userAnswer) { // Compare with the global answer variable
     output.innerText = `Correct! The answer was ${answer}!`;
     counterUp();
-
-
+    makeQuestion();
+    document.querySelector('#input-area').value = ''
   } else {
-    output.innerText = 'Incorrect....';
+    output.innerText = `${answer} was incorrect....`;
     counterReset();
+    document.querySelector('#input-area').value = ''
   }
 }
 
@@ -60,6 +75,7 @@ function counterUp() {
   if (parseInt(counter.innerText) > highScore) {
     highScore = parseInt(counter.innerText);
     document.querySelector('#high_score').innerText = highScore;
+    localStorage.setItem('highScore', highScore); // Save high score to local storage
   }
 }
 
@@ -86,5 +102,4 @@ function startTimer(duration, display) {
       counterReset();
     }
   }, 1000);
-
 }
